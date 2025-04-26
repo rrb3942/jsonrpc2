5211cc21cd33523c8dc92599019125d59196610b
Ryan Bullock <ryan@piratel.com>
2025-04-26 12:34:42 -0700

    Doc update, Deadliner includes io.Closer

--- deadliner.go
+++ deadliner.go
@@ -5,13 +5,13 @@ import (
 	"time"
 )
 
-// DeadlineReader represents an [io.Reader] that supports setting a deadline.
+// DeadlineReader represents an [io.ReaderCloser] that supports setting a deadline.
 type DeadlineReader interface {
 	io.ReadCloser
 	SetReadDeadline(time.Time) error
 }
 
-// DeadlineWrite represents an [io.Write] that supports setting a deadline.
+// DeadlineWrite represents an [io.WriteCloser] that supports setting a deadline.
 type DeadlineWriter interface {
 	io.WriteCloser
 	SetWriteDeadline(time.Time) error
